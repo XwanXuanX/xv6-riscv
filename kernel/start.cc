25dee6973188ad4aa5434ddb79894556e8bf57b1
Wanxuan Li <lzlu1018@gmail.com>
2026-02-15 03:20:08 -0500

    type
    Signed-off-by: Wanxuan Li <lzlu1018@gmail.com>

--- kernel/start.cc
+++ kernel/start.cc
@@ -42,7 +42,7 @@ extern "C" void start() {
     timerinit();
 
     // keep each CPU's hartid in its tp register, for cpuid().
-    int id = r_mhartid();
+    uint64 id = r_mhartid();
     w_tp(id);
 
     // switch to supervisor mode and jump to main().
