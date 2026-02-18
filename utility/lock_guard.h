f55f7b440c56f1ed21b54371b6504da69f4dcd8f
Wanxuan Li <lzlu1018@gmail.com>
2026-02-17 21:56:32 -0500

    own lock_guard
    Signed-off-by: Wanxuan Li <lzlu1018@gmail.com>

---
+++ utility/lock_guard.h
@@ -0,0 +1,21 @@
+// Self implementation of std::lock_guard with RAII style lock management
+#pragma once
+
+namespace xv6::util {
+
+template <typename Mutex>
+class lock_guard {
+public:
+    explicit lock_guard(Mutex& m) : m_(m) {
+        m_.lock();
+    }
+
+    ~lock_guard() {
+        m_.unlock();
+    }
+
+private:
+    Mutex& m_;
+};
+
+}
