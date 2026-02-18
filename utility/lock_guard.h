23719a20b45d1b366617ec57af6a0beb5d6dc93c
Wanxuan Li <lzlu1018@gmail.com>
2026-02-17 22:38:01 -0500

    lockable concept
    Signed-off-by: Wanxuan Li <lzlu1018@gmail.com>

--- utility/lock_guard.h
+++ utility/lock_guard.h
@@ -1,9 +1,17 @@
 // Self implementation of std::lock_guard with RAII style lock management
 #pragma once
 
+#include <concepts>
+
 namespace xv6::util {
 
-template <typename Mutex> class lock_guard {
+template <typename T>
+concept lockable = requires(T m) {
+    { m.lock() } -> std::same_as<void>;
+    { m.unlock() } -> std::same_as<void>;
+};
+
+template <lockable Mutex> class lock_guard {
   public:
     explicit lock_guard(Mutex &m) : m_(m) { m_.lock(); }
 
