4cb520daa150c28b1b63716efcf713b76179b5ce
Wanxuan Li <lzlu1018@gmail.com>
2026-02-17 23:04:24 -0500

    new sleeplock
    Signed-off-by: Wanxuan Li <lzlu1018@gmail.com>

--- kernel/sleeplock.h
+++ kernel/sleeplock.h
@@ -5,13 +5,29 @@
 namespace xv6 {
 
 // Long-term locks for processes
-struct sleeplock {
-    uint locked; // Is the lock held?
-    spinlock lk; // spinlock protecting this sleep lock
+class sleeplock {
+  public:
+    sleeplock() = default;
+
+    // Initialize the lock
+    void init_lock();
+
+    // Acquire the lock
+    void lock();
+
+    // Release the lock
+    void unlock();
+
+    // Is anyone holding the lock
+    [[nodiscard]] bool holding();
+
+  private:
+    uint locked_; // Is the lock held?
+    spinlock lk_; // spinlock protecting this sleep lock
 
     // For debugging:
-    char *name; // Name of lock.
-    int pid;    // Process holding lock
+    const char *name_; // Name of lock.
+    int pid_;          // Process holding lock
 };
 
 } // namespace xv6
