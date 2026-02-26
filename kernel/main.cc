1206bc9a8154e5367ea66e1788de9c361c04f0f6
Wanxuan Li <lzlu1018@gmail.com>
2026-02-26 16:20:52 -0500

    self test ts_list in on boot

--- kernel/main.cc
+++ kernel/main.cc
@@ -3,6 +3,7 @@
 #include "kernel/kalloc.h"
 #include "kernel/slab.h"
 #include "kernel/stl/ts_forward_list.h"
+#include "kernel/stl/ts_list.h"
 #include "kernel/kstack_alloc.h"
 
 namespace xv6 {
@@ -13,6 +14,10 @@ static void self_test() {
     test::slab_self_test();            // slab allocator self test
     test::ts_forward_list_self_test(); // thread-safe list self test
     test::kstack_self_test();          // kernel stack allocator self test
+    test::ts_list_self_test();         // thread-safe doubly list self test
+
+    // after all self tests, reclaim all slab's physical pages
+    slabs_reclaim();
 }
 
 // start() jumps here in supervisor mode on all CPUs.
@@ -40,7 +45,7 @@ void main() {
         auto &feedback_q = multi_lvl_feedback_q::instance();
         feedback_q.init(); // initialize MLFQ
         slabs_init();      // initialize all slab allocators
-        self_test();       // self test subsystems
+        self_test();       // subsystems self tests
         userinit();        // first user process
         __sync_synchronize();
         started = 1;
